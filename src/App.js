6c21ed8e0e4122979600c1a84ec7b24a69278731
ranjeet patil <ranjeetpatil3698@github.com>
2021-07-30 15:50:59 +0530

    deploy ready

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
-import './App.css';
-import RenderForm from './components/pages/RenderForm';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import CreateForm from "./components/pages/CreateForm"
+import "./App.css";
+import RenderForm from "./components/pages/RenderForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CreateForm from "./components/pages/CreateForm";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
-import Admin from './components/Admin';
-import Response from './components/Response';
-import PrivateRoute from './components/PrivateRoute';
-import Home from './components/Home';
-import Sentresponse from './components/Sentresponse';
+import Admin from "./components/Admin";
+import Response from "./components/Response";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./components/Home";
+import Sentresponse from "./components/Sentresponse";
 import { useSelector } from "react-redux";
-import Errorpage from './components/Errorpage';
-import Viewfile from './components/Viewfile';
+import Errorpage from "./components/Errorpage";
+import Viewfile from "./components/Viewfile";
 
-const App=() =>{
-
-  const { formname,formsubmit } = useSelector(
-    (state) => state.formrender
-  );
+const App = () => {
+  const { formname, formsubmit } = useSelector((state) => state.formrender);
 
   return (
     <Router>
-      <div >
+      <div>
         <Switch>
-          <Route path='/' component={Home} exact={true}/>
-          <Route path="/viewform/:url" component={RenderForm}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/signup" component={SignUp}/>
-          {formsubmit&&<Route path="/thankyou"><Sentresponse formname={formname}/></Route>}
-          <PrivateRoute path="/admin" >
-            <Admin/>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/viewform/:url" component={RenderForm} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          {formsubmit && (
+            <Route path="/thankyou">
+              <Sentresponse formname={formname} />
+            </Route>
+          )}
+          <PrivateRoute path="/admin">
+            <Admin />
           </PrivateRoute>
-          <PrivateRoute path="/responses/:url" >
-            <Response/>
+          <PrivateRoute path="/responses/:url">
+            <Response />
           </PrivateRoute>
           <PrivateRoute path="/createform">
-            <CreateForm/>
+            <CreateForm />
           </PrivateRoute>
           <PrivateRoute path="/viewfile/:filename">
-            <Viewfile/>
+            <Viewfile />
           </PrivateRoute>
-          <Route path='*' component={Errorpage} />
+          <Route path="*" component={Errorpage} />
         </Switch>
-        
       </div>
     </Router>
-    
   );
-}
+};
 
 export default App;
